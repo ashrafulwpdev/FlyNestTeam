538105e82c6ecb54ac55a64a505dcf6effdb6009
ashrafulwpdev <ashrafulwpdev@gmail.com>
2025-04-11 02:53:43 +0800

    Ui Imove 2.01

--- app/src/main/java/com/group/FlyNest/model/Passenger.kt
+++ app/src/main/java/com/group/FlyNest/model/Passenger.kt
@@ -5,9 +5,10 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Passenger(
-    val name: String = "",
-    val email: String = "",
-    val phone: String = "",
-    val passport: String = "",
-    val isPrimary: Boolean = false
+    val name: String,
+    val email: String,
+    val phone: String,
+    val passport: String,
+    val isPrimary: Boolean = true,
+    val specialRequests: String? = null
 ) : Parcelable
